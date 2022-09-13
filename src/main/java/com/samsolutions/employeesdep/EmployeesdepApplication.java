97b563dc957d782ef6b1bd6d8e3cfd15ec1a6bc1
Alec Kotovich <A.Katovich@sam-solutions.com>
2022-09-13 14:01:17 +0300

    Initial implementation

---
+++ src/main/java/com/samsolutions/employeesdep/EmployeesdepApplication.java
@@ -0,0 +1,13 @@
+package com.samsolutions.employeesdep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesdepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesdepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsolutions/employeesdep/EmployeesdepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsolutions.employeesdep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesdepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
