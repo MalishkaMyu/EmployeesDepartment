f32fb62368524626dea632f6195d55431160bccf
mariy <maliska_myu@mail.ru>
2023-04-06 11:37:36 +0300

    Security: Embedded keycloak server

--- src/main/java/com/samsolutions/employeesdep/EmployeesdepApplication.java
+++ src/main/java/com/samsolutions/employeesdep/EmployeesdepApplication.java
@@ -1,23 +1,34 @@
 package com.samsolutions.employeesdep;
 
+import com.samsolutions.employeesdep.config.KeycloakServerProperties;
 import com.samsolutions.employeesdep.config.MyPasswordEncoder;
 import com.samsolutions.employeesdep.model.entities.User;
 import com.samsolutions.employeesdep.model.repository.UserRepository;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+@EnableConfigurationProperties({ KeycloakServerProperties.class })
 public class EmployeesdepApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeesdepApplication.class, args);
     }
+
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeesdepApplication.class);
 
     @Value("${spring.flyway.placeholders.admin_login}")
     private String adminLogin;
@@ -27,6 +38,19 @@ public class EmployeesdepApplication {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties,
+                                                                               KeycloakServerProperties keycloakServerProperties) {
+
+        return (evt) -> {
+
+            Integer port = serverProperties.getPort();
+            String keycloakContextPath = keycloakServerProperties.getContextPath();
+
+            LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak", port, keycloakContextPath);
+        };
+    }
 
     @PostConstruct
     public void generateAdminPassword() {
---
+++ src/main/java/com/samsolutions/employeesdep/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,6 @@
+package com.samsolutions.employeesdep.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+}
